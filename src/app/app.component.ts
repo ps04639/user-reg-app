302fb779b99cfc6508b4fc10417d1699444fb73b
Prashant Kumar <ps04639@gmail.com>
2019-07-15 02:58:08 +0530

    code update

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'user-reg-app';
+}
